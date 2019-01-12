808a0b7ac31aa1ef477e71b6b7108656add0bf25
WuYuanhun <1041747729@qq.com>
2019-01-12 21:54:48 +0800

    update

--- main.js
+++ main.js
@@ -4,46 +4,59 @@
 
 var client = AgoraRTC.createClient({mode: 'live', codec: "h264"});
 
-client.init( appKey , function () {
-  console.log("AgoraRTC client initialized");
+function start() {
+  client.init( appKey , function () {
+    console.log("AgoraRTC client initialized");
+  
+  }, function (err) {
+    console.log("AgoraRTC client init failed", err);
+  });
+  
+  appKey = null;
+  
+  client.join(null, "1000", 999, function(uid) {
+    console.log("User " + uid + " jion channel Successfully!");
+  }, function(err) {
+    console.log("Join channel failed.", err);
+  });
+  
+  
+  var localStream = AgoraRTC.createStream({
+    streaID: 999,
+    audio: true,
+    video: true,
+    screen: false
+  });
+  
+  AgoraRTC.getDevices (function(devices){
+    var decC = devices.length;
+  
+    var id = devices[0].devicesId;
+    console.log("Devices: ");
+    console.log(devices);
+  });
+  
+  localStream.init(function() {
+    console.log("getUserMedia successfully!");
+    localStream.play('agora_local');
+  }, function (err) {
+    console.log("getUserMeida failed: ", err);
+  });
+  
 
-}, function (err) {
-  console.log("AgoraRTC client init failed", err);
-});
+  client.publish(localStream, function(err) {
+    console.log("Publish local stream error: " + err);
+  });
 
-appKey = null;
+  client.on('stream-published', function (evt) {
+    console.log("Publish local mstream successfully!");
+  }); 
+}
 
-client.join(null, "1000", 999, function(uid) {
-  console.log("User " + uid + " jion channel Successfully!");
-}, function(err) {
-  console.log("Join channel failed.", err);
-});
-
-
-var localStream = AgoraRTC.createStream({
-  streaID: 999,
-  audio: true,
-  video: true,
-  screen: false
-});
-
-AgoraRTC.getDevices (function(devices){
-  var decC = devices.length;
-
-  var id = devices[0].devicesId;
-});
-
-localStream.init(function() {
-  console.log("getUserMedia successfully!");
-  localStream.play('agora_local');
-}, function (err) {
-  console.log("getUserMeida failed: ", err);
-});
-
-client.publish(localStream, function(err) {
-  console.log("Publish local stream error: " + err);
-});
-
-client.on('stream-published', function (evt) {
-  console.log("Publish local mstream successfully!");
-}); 
+function stop() {
+  client.leave(function() {
+    console.log("Client leaves channel");
+  }, function(err) {
+    console.log("client leave failed ", err);
+  }); 
+}
